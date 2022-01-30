8239d8358580bd307ca938d3144c5682ae96c7a5
DilshanHarendra <harendracripto@gmail.com>
2022-01-30 23:39:26 +0530

    start creating plugin

---
+++ simple-form.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin name : simple-form
+ * Description : this is a test plugin
+ * Author : Dilshan
+ * Author URI : https://github.com/DilshanHarendra/wordpress-simple-form
+ * Version : 1.0.0
+ * Test Domain : Simple Form
+ *
+ *
+ *
+ * */
